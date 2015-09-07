66f3b605987e45ea59eb34dbba1f8bb0668865b2
Jixi Aeons <jixion@gmail.com>
2015-09-07 17:42:41 -0400

    initial commit

---
+++ startStreaming.sh
@@ -0,0 +1,7 @@
+echo "Starting Motion Detector and Streaming Daemon..."
+sudo motion
+sleep 5
+echo "Motion Detection started"
+sleep 5
+echo "Streaming to marvel..."
+ssh -CNR 8081:localhost:8081 marvel
---
+++ startTunnel.sh
@@ -0,0 +1,2 @@
+ssh -CNfR 41122:localhost:22 marvel
+echo "Open to marvel..."
